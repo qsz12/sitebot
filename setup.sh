7f3ddaa7afff92d31a757ab7f371631cefd1aaa1
Suraj Zala <suraj.zala@smartsensesolutions.com>
2023-01-10 19:47:34 +0530

    Update: added missing file

--- setup.sh
+++ setup.sh
@@ -3,7 +3,5 @@ exec `sudo apt install git build-essential`
 export EC2_AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
 export EC2_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed 's/[a-z]$//'`"
 
-exec `git clone https://github.com/essnine/sitebot`
-
-cd sitebot
-exec `pip install -r requirements.txt`
+exec `pip3 install -r requirements.txt`
+exec `gunicorn app:app -k=eventlet --bind=0.0.0.0:8080 --access-logfile=- --log-level=INFO`
