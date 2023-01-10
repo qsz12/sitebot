4e007758bab69ee876fecae93778f820512f864f
Suraj Zala <suraj.zala@smartsensesolutions.com>
2023-01-10 19:50:42 +0530

    Update: more change

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ export EC2_AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement
 export EC2_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed 's/[a-z]$//'`"
 
 exec `pip3 install -r requirements.txt`
-exec `gunicorn app:app -k=eventlet --bind=0.0.0.0:8080 --access-logfile=- --log-level=INFO`
+exec `gunicorn app:app -k=eventlet --bind=0.0.0.0:8080 --access-logfile=- --log-level=INFO &`
