abe80df65592be91b99f7ee5c889e6ea83835774
Suraj Zala <suraj.zala@smartsensesolutions.com>
2023-01-10 19:52:11 +0530

    Update: don't ask

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-exec `sudo apt install git build-essential`
+exec `sudo apt update & sudo apt install git build-essential`
 
 export EC2_AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
 export EC2_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed 's/[a-z]$//'`"
