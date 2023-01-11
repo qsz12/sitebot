c1b0831d465f9a70806ce2da358ab0c83d1e4e8f
Suraj Zala <suraj.zala@smartsensesolutions.com>
2023-01-11 15:10:10 +0530

    Fixes to setup script

--- setup.sh
+++ setup.sh
@@ -2,13 +2,13 @@
 
 # running apt update and upgrade, to be sure everything is up to the mark
 sudo apt update -y
-sudo apt install git build-essential -y
+sudo apt install git build-essential python3-pip -y
 
 # setting an EC2 env var
 export EC2_AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
 export EC2_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed 's/[a-z]$//'`"
 
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 
 # running the server and detaching
 gunicorn app:app -k=eventlet --bind=0.0.0.0:8080 --access-logfile=- --log-level=INFO &
