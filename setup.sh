04d739dc74eaa7046cdf5f4aac033b1225d172de
Suraj Zala <suraj.zala@smartsensesolutions.com>
2023-01-11 15:08:48 +0530

    Updated setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
-exec `sudo apt update -y`
-exec `sudo apt install git build-essential -y`
+#!/usr/bin/env bash
 
+# running apt update and upgrade, to be sure everything is up to the mark
+sudo apt update -y
+sudo apt install git build-essential -y
+
+# setting an EC2 env var
 export EC2_AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
 export EC2_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed 's/[a-z]$//'`"
 
-exec `pip3 install -r requirements.txt`
-exec `gunicorn app:app -k=eventlet --bind=0.0.0.0:8080 --access-logfile=- --log-level=INFO &`
+pip3 install -r requirements.txt
+
+# running the server and detaching
+gunicorn app:app -k=eventlet --bind=0.0.0.0:8080 --access-logfile=- --log-level=INFO &
